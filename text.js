26cad7dc265655e66a4ca1da3eeb8998fbb9e7b8
IcoderGameDev <63498344+IcoderGameDev@users.noreply.github.com>
2021-05-19 09:04:23 +0200

    uploaded rest of files

--- text.js
+++ text.js
@@ -2,16 +2,30 @@ class Text extends GameObject{
 	constructor(x,y,text) {
 	super(x,y);
 	this.text = text;
+	this.clr = 0;
+	this.width = textWidth(this.text);
+	this.height = 10;
 	this.typeId = 3;
 	}
+	getClassName() {
+	return "Text"
+	}
 	getValues() {
-	return[...super.getValues(),"\"" + this.text + "\""];
+	return[...super.getValues(),"\"" + this.t + "\""];
 	}
 	getValuesName() {
 	return[...super.getValuesName(),"text"]
 	}
+	getActualValuesName() {
+	return [...super.getActualValuesName(),"text"]
+	}
+	set text(v) {
+	this.t = v;
+	this.width = textWidth(this.t);
+	}
 	display() {
-	text(this.text,this.x,this.y);
+	fill(this.clr)
+	text(this.t,this.x,this.y);
 	}
 	
 }
