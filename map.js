80e46709f8a604885810581ff51368363f79eac6
DarK <63498344+DarKDevz@users.noreply.github.com>
2023-06-03 19:52:19 +0200

    Added Add Component button

--- map.js
+++ map.js
@@ -5,41 +5,41 @@ var classes = {
     End: End,
     Enemy: Enemy,
     Interact: Interactive
-}
-var makingNew = false,
-    valChanged = new Event("ValueChanged"),
-    newBox = null,
-    pasted = false,
-    copiedObjs = [],
-    cameraPos = null,
-    lastWasPressed = false,
-    overUI = false,
-    Pressed = lastWasPressed,
-    button = null,
-    selectBox = [],
-    Playing = null,
-    Paused = null,
-    pauseButton = null,
-    addButton = null,
-    selectObject = null,
-    selectedObjects = [],
-    lastScene = null,
-    inputFile = null,
-    saveButton = null,
-    copyButton = null,
-    removeButton = null,
-    levelButton = null,
-    levelMode = false,
-    actionButtons = null,
-    sideMenu = null,
-    boxInfo = null,
-    info = [],
-    LastInfo = [],
-    infoDivs = [],
-    infoDivsHolder = [],
-    infoIndexes = [],
-    addSelect = null,
-    id = null;
+};
+var makingNew = false
+  , valChanged = new Event("ValueChanged")
+  , newBox = null
+  , pasted = false
+  , copiedObjs = []
+  , cameraPos = null
+  , lastWasPressed = false
+  , overUI = false
+  , Pressed = lastWasPressed
+  , button = null
+  , selectBox = []
+  , Playing = null
+  , Paused = null
+  , pauseButton = null
+  , addButton = null
+  , selectObject = null
+  , selectedObjects = []
+  , lastScene = null
+  , inputFile = null
+  , saveButton = null
+  , copyButton = null
+  , removeButton = null
+  , levelButton = null
+  , levelMode = false
+  , actionButtons = null
+  , sideMenu = null
+  , boxInfo = null
+  , info = []
+  , LastInfo = []
+  , infoDivs = []
+  , infoDivsHolder = []
+  , infoIndexes = []
+  , addSelect = null
+  , id = null;
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
@@ -67,8 +67,8 @@ function uiButton(name, x, y) {
 }
 
 function uiElement(element) {
-    element.mouseOver(() => overUI = true);
-    element.mouseOut(() => overUI = false);
+    element.mouseOver(()=>overUI = true);
+    element.mouseOut(()=>overUI = false);
 }
 
 function setup() {
@@ -84,18 +84,25 @@ function setup() {
     uiElement(inputFile);
 
     addButton = uiButton('add New', 75, 0);
-    addButton.mousePressed(() => { makingNew = !makingNew; })
+    addButton.mousePressed(()=>{
+        makingNew = !makingNew;
+    }
+    )
     addSelect = createSelect();
     addSelect.position(0, 0);
-    Object.keys(classes).forEach(element => {
+    Object.keys(classes).forEach(element=>{
         addSelect.option(element)
-    });
+    }
+    );
     saveButton = uiButton("Save", 220, 0);
     saveButton.mousePressed(saveMap);
     uiElement(saveButton);
 
     pauseButton = uiButton('Paused', windowWidth / 2, 0);
-    pauseButton.mousePressed(() => { Paused = !Paused });
+    pauseButton.mousePressed(()=>{
+        Paused = !Paused
+    }
+    );
     sideMenu = createDiv();
     sideMenu.size(300);
     sideMenu.style("max-height:calc(100vh - 20px);overflow:auto;height:fit-content;background-color: rgba(0, 0, 0, 0.25);");
@@ -124,10 +131,11 @@ function setup() {
 
     JsonMap(MapData);
 
-    button.mousePressed(() => {
+    button.mousePressed(()=>{
         levels[activeLevel].loadLevel();
         Playing = !Playing;
-    });
+    }
+    );
 
     lastScene = activeLevel;
     cameraPos = createVector(0, 0);
@@ -144,11 +152,12 @@ function levelScreen() {
         let LValueNames = levels[activeLevel].getLevelValueNames();
         let LValueIndx = levels[activeLevel].getActualLevelValues();
         for (let i = 0; i < LValues.length; i += 1) {
-            addMenuInput(LValueNames[i], (val) => {
+            addMenuInput(LValueNames[i], (val)=>{
                 let actValue = parseInt(val) ? parseInt(val) : val
                 levels[activeLevel][LValueIndx[i]] = actValue;
                 LValues[i] = actValue;
-            }, () => LValues[i])
+            }
+            , ()=>LValues[i])
         }
     }
 }
@@ -161,14 +170,17 @@ removeObject = function(id) {
 function removeAndDecrease(arr, value) {
     const index = arr.indexOf(value.toString());
     if (index !== -1) {
-        arr.splice(index, 1); // Remove the value from the array
+        arr.splice(index, 1);
+        // Remove the value from the array
         for (let i = index; i < arr.length; i++) {
-            arr[i] = (parseInt(arr[i], 10) - 1).toString(); // Decrease subsequent values by 1
+            arr[i] = (parseInt(arr[i], 10) - 1).toString();
+            // Decrease subsequent values by 1
         }
     } else {
         for (let i = 0; i < arr.length; i++) {
             if (parseInt(arr[i], 10) > parseInt(value, 10)) {
-                arr[i] = (parseInt(arr[i], 10) - 1).toString(); // Decrease subsequent values by 1
+                arr[i] = (parseInt(arr[i], 10) - 1).toString();
+                // Decrease subsequent values by 1
             }
         }
     }
@@ -182,13 +194,15 @@ function removeMapObject() {
         delete selectedObjects[selectedId];
     }
     //filter empty
-    levels[activeLevel].boxes = getCurrentBoxes().filter((_) => {
+    levels[activeLevel].boxes = getCurrentBoxes().filter((_)=>{
         return _
-    })
+    }
+    )
     levels[activeLevel].reloadBoxes();
-    selectedObjects = selectedObjects.filter((_) => {
+    selectedObjects = selectedObjects.filter((_)=>{
         return _
-    })
+    }
+    )
 }
 
 function copyObject() {
@@ -213,7 +227,8 @@ function pasteObjects() {
             levels[activeLevel].boxes[index].clr = 50;
             let offsetPosX = mouseCoords().x;
             let offsetPosY = mouseCoords().y;
-            if (!firstObjPos) firstObjPos = [obj.x, obj.y];
+            if (!firstObjPos)
+                firstObjPos = [obj.x, obj.y];
             else {
                 offsetPosX -= firstObjPos[0] - obj.x;
                 offsetPosY -= firstObjPos[1] - obj.y;
@@ -236,21 +251,29 @@ function draw() {
     clear();
     background(150, 230, 240);
     /*-------------PLAYER AND LEVEL DRAWING-----------------*/
-    if (Playing && !Paused) player.update();
+    if (Playing && !Paused)
+        player.update();
     //Early Update
-    if (!Paused) levels[activeLevel].earlyUpdate();
-    if (Playing && !Paused) player.camera();
-    else translate(cameraPos.x, cameraPos.y)
-    if (Playing && !Paused) player.checkCollisions();
+    if (!Paused)
+        levels[activeLevel].earlyUpdate();
+    if (Playing && !Paused)
+        player.camera();
+    else
+        translate(cameraPos.x, cameraPos.y)
+    if (Playing && !Paused)
+        player.checkCollisions();
     levels[activeLevel].display();
-    if (levelMode) levels[activeLevel].customDraw();
-    if (Playing) player.display();
+    if (levelMode)
+        levels[activeLevel].customDraw();
+    if (Playing)
+        player.display();
     //Late Update
-    if (!Paused) levels[activeLevel].lateUpdate();
+    if (!Paused)
+        levels[activeLevel].lateUpdate();
     /*-------------PLAYER AND LEVEL DRAWING-----------------*/
     //DRAW SELECT BOX
     if (selectBox[1]) {
-        let rect1 = new Box(selectBox[0][0], selectBox[0][1], selectBox[1][0] - selectBox[0][0], selectBox[1][1] - selectBox[0][1]);
+        let rect1 = new Box(selectBox[0][0],selectBox[0][1],selectBox[1][0] - selectBox[0][0],selectBox[1][1] - selectBox[0][1]);
         fill(0, 0, 0, 25);
         rect(rect1.x, rect1.y, rect1.width, rect1.height);
     }
@@ -343,14 +366,19 @@ function OpenEditMenu() {
                 info.push(components.componentName);
             }
         }
+        info.push(objectId);
+        info.push("CustomButton");
+        info.push(0);
+        info.push(0);
     }
-    if((lastInfo.length!==info.length))console.log(lastInfo.length-info.length,lastIndexes.length-infoIndexes.length);
+    if ((lastInfo.length !== info.length))
+        console.log(lastInfo.length - info.length, lastIndexes.length - infoIndexes.length);
     if (info.equals(lastInfo)) {
         return;
     }
-    let newInfo = lastInfo.length!==info.length
+    let newInfo = lastInfo.length !== info.length
     let noNewObjects = lastIndexes.equals(infoIndexes)
-    if (!newInfo&&noNewObjects) {
+    if (!newInfo && noNewObjects) {
         //edit existing values
         for (let t_info of infoDivs) {
             t_info.elt.dispatchEvent(valChanged);
@@ -367,28 +395,45 @@ function OpenEditMenu() {
     console.table(info);
     for (let i = 0; i < info.length; i += 4) {
         console.log(info[i]);
-        if (info[i + 1] === "noMenu" || info[i + 1] === "component") {
+        if (info[i + 1] === "noMenu" || info[i + 1] === "component" || info[i + 1] === "CustomButton") {
             console.log("works");
-            if (info[i + 1] === "noMenu") {
-                // if (boxes[info[i]].components[info[i + 2]]) {
-                //     boxes[info[i]].components[info[i + 2]].MenuEdit('sideMenu');
-                // }
-            } else {
+            if (info[i + 1] === "noMenu") {// if (boxes[info[i]].components[info[i + 2]]) {
+            //     boxes[info[i]].components[info[i + 2]].MenuEdit('sideMenu');
+            // }
+            } else if (info[i + 1] === "component") {
                 if (boxes[info[i]].components[info[i + 2]]) {
                     boxes[info[i]].components[info[i + 2]].MenuEdit('sideMenu');
                 }
+            } else {
+                let divHolder = createDiv();
+                let ComponentSelect = createSelect();
+                for (const [key,value] of Object.entries(componentList)) {
+                    ComponentSelect.option(key);
+                }
+                ComponentSelect.parent(divHolder);
+                let addButton = createButton("Add");
+                addButton.mousePressed(()=>{
+                    boxes[info[i]].components.push(new componentList[ComponentSelect.value()]({
+                        obj: boxes[info[i]]
+                    }))
+                }
+                )
+                addButton.parent(divHolder)
+                divHolder.parent('sideMenu')
+                infoDivs.push(divHolder);
             }
         } else {
-            addMenuInput(info[i + 1], (val) => {
+            addMenuInput(info[i + 1], (val)=>{
                 let actValue = parseInt(val) ? parseInt(val) : val
                 boxes[info[i]][info[i + 3]] = actValue;
                 info[i + 2] = actValue;
-            }, () => info[i + 2])
+            }
+            , ()=>info[i + 2])
         }
     }
 }
 
-function addEditableScript(name, set, get, parentName = "sideMenu") {
+function addEditableScript(name, set, get, parentName="sideMenu") {
     let divHolder = createDiv();
     let headerText = createSpan("►Script Component").parent(divHolder);
     let isExpanded = false;
@@ -400,24 +445,23 @@ function addEditableScript(name, set, get, parentName = "sideMenu") {
     inputField.hide();
 
     let _span = createSpan(name + ": ").parent(inputField);
-    _span.style('margin-left', '1em'); // Add a tab space using CSS margin-left
+    _span.style('margin-left', '1em');
+    // Add a tab space using CSS margin-left
 
     let inp = createButton("Script").parent(inputField);
-    inp.mousePressed(() => {
-        var popupWindow = window.open(
-            "popup.html?text=" + encodeURIComponent(_get.toString()),
-            "Popup Window",
-            "width=400,height=300"
-        );
+    inp.mousePressed(()=>{
+        var popupWindow = window.open("popup.html?text=" + encodeURIComponent(_get.toString()), "Popup Window", "width=400,height=300");
         // Receive updated text from the popup window
-        window.receivePopupText = (text) => {
+        window.receivePopupText = (text)=>{
             console.log(text);
             _get = set(text);
-        };
-    });
+        }
+        ;
+    }
+    );
     inp.size(177, 21);
 
-    divHolder.mousePressed(() => {
+    divHolder.mousePressed(()=>{
         isExpanded = !isExpanded;
         if (isExpanded) {
             headerText.html("▼Script Component");
@@ -426,19 +470,21 @@ function addEditableScript(name, set, get, parentName = "sideMenu") {
         } else {
             headerText.html("►Script Component");
             inputField.style("max-height", "0px");
-            setTimeout(() => {
+            setTimeout(()=>{
                 inputField.hide();
-            }, 200); // Adjust the timeout value to match the CSS transition duration
+            }
+            , 200);
+            // Adjust the timeout value to match the CSS transition duration
         }
-    });
+    }
+    );
 
     infoDivs.push(divHolder);
     infoDivs[infoDivs.length - 1].parent(parentName);
     inputField.parent(divHolder);
 }
 
-
-function addEditableSprite(name, set, get, parentName = "sideMenu") {
+function addEditableSprite(name, set, get, parentName="sideMenu") {
     let divHolder = createDiv();
     let headerText = createSpan("►Sprite Component").parent(divHolder);
     let _get = get;
@@ -451,11 +497,12 @@ function addEditableSprite(name, set, get, parentName = "sideMenu") {
     inputField.hide();
 
     let _span = createSpan(name + ": ").parent(inputField);
-    _span.style('margin-left', '1em'); // Add a tab space using CSS margin-left
+    _span.style('margin-left', '1em');
+    // Add a tab space using CSS margin-left
 
     let inp = createButton("Sprite").parent(inputField);
 
-    headerText.mousePressed(() => {
+    headerText.mousePressed(()=>{
         isExpanded = !isExpanded;
         if (isExpanded) {
             headerText.html("▼Sprite Component");
@@ -464,20 +511,25 @@ function addEditableSprite(name, set, get, parentName = "sideMenu") {
         } else {
             headerText.html("►Sprite Component");
             inputField.style("max-height", "0px");
-            setTimeout(() => {
+            setTimeout(()=>{
                 inputField.hide();
-            }, 200); // Adjust the timeout value to match the CSS transition duration
+            }
+            , 200);
+            // Adjust the timeout value to match the CSS transition duration
         }
-    });
+    }
+    );
 
-    inp.mousePressed(() => {
+    inp.mousePressed(()=>{
         let popup = window.open('imagePopup.html', '_blank', 'width=400,height=400');
         // Receive updated text from the popup window
-        window.jsonImage = (text) => {
+        window.jsonImage = (text)=>{
             console.log(text);
             _get = set(text);
-        };
-    });
+        }
+        ;
+    }
+    );
 
     inp.size(177, 21);
     let infoId = infoDivs.push(divHolder);
@@ -485,20 +537,19 @@ function addEditableSprite(name, set, get, parentName = "sideMenu") {
     inputField.parent(divHolder);
 }
 
-
-
-
 function addMenuInput(name, set, get) {
     let divHolder = createDiv();
     divHolder.html();
     let _span = createSpan(name + ": ").parent(divHolder);
     let inp = createInput(get().toString()).style("opacity:0.5;")
-    inp.parent(divHolder).input(() => {
+    inp.parent(divHolder).input(()=>{
         set(inp.value());
-    });
-        divHolder.elt.addEventListener("ValueChanged",
-                                   ()=>{inp.value(get())}
-                                  );
+    }
+    );
+    divHolder.elt.addEventListener("ValueChanged", ()=>{
+        inp.value(get())
+    }
+    );
     infoDivs.push(divHolder);
     infoDivs[infoDivs.length - 1].parent('sideMenu')
 }
@@ -506,10 +557,7 @@ function addMenuInput(name, set, get) {
 function editMenuInput(index, name, set, get) {}
 
 function mouseCoords() {
-    return createVector(
-        round(Playing && !Paused ? mouseX + player.cameraPos.x : mouseX - cameraPos.x),
-        round(Playing && !Paused ? mouseY + player.cameraPos.y : mouseY - cameraPos.y)
-    )
+    return createVector(round(Playing && !Paused ? mouseX + player.cameraPos.x : mouseX - cameraPos.x), round(Playing && !Paused ? mouseY + player.cameraPos.y : mouseY - cameraPos.y))
 }
 
 function transformCoordinates(drawSelect) {
@@ -535,21 +583,25 @@ function transformCoordinates(drawSelect) {
 }
 
 function mouseUp() {
-    if (!selectBox[0]) return;
+    if (!selectBox[0])
+        return;
     selectBox[1] = [mouseCoords().x, mouseCoords().y];
     let drawSelect = selectBox;
     let rect1;
     rect1 = new Box(...transformCoordinates(drawSelect));
 
-    if (!rect1) return;
-    if (makingNew) return newBox = rect1;
+    if (!rect1)
+        return;
+    if (makingNew)
+        return newBox = rect1;
     selectedObjects = [];
     for (t_box_id in boxes) {
         let t_box = boxes[t_box_id];
         let c = t_box.collision(rect1, false);
-        if (c) selectedObjects.push(t_box_id);
+        if (c)
+            selectedObjects.push(t_box_id);
         t_box.clr = c * 50
-            //console.log(c);
+        //console.log(c);
     }
 }
 // Warn if overriding existing method
@@ -557,28 +609,28 @@ if (Array.prototype.equals)
     console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
 // attach the .equals method to Array's prototype to call it on any array
 Array.prototype.equals = function(array) {
-        // if the other array is a falsy value, return
-        if (!array)
-            return false;
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
 
-        // compare lengths - can save a lot of time 
-        if (this.length != array.length)
-            return false;
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length)
+        return false;
 
-        for (var i = 0, l = this.length; i < l; i++) {
-            // Check if we have nested arrays
-            if (this[i] instanceof Array && array[i] instanceof Array) {
-                // recurse into the nested arrays
-                if (!this[i].equals(array[i]))
-                    return false;
-            } else if (this[i] != array[i]) {
-                // Warning - two different object instances will never be equal: {x:20} != {x:20}
+    for (var i = 0, l = this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i]instanceof Array && array[i]instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
                 return false;
-            }
+        } else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
         }
-        return true;
     }
-    // Hide method from for-in loops
+    return true;
+}
+// Hide method from for-in loops
 Object.defineProperty(Array.prototype, "equals", {
     enumerable: false
 });
