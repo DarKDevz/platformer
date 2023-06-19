9212ab01566c81889ebbb6133f02d3530f37c525
DarK <63498344+DarKDevz@users.noreply.github.com>
2023-06-19 11:07:48 +0200

    Some Things haven't been updated

--- sketch.js
+++ sketch.js
@@ -3,6 +3,12 @@ function windowResized() {
 }
 function setup() {
 	createCanvas(windowWidth,windowHeight);
+	//Remove right click default behaviour
+	canvas.oncontextmenu = function(e) {
+		e.preventDefault();
+	}
+	noSmooth()
+
 	player = new Player();
 	engine = new Engine();
 	JsonMap(MapData)
@@ -13,18 +19,14 @@ function draw() {
 	clear();
 	background(150, 230, 240);
 	//Update
-	player.update();
+	player.update(!0);
 	//Early Update
-	engine.getActiveScene().earlyUpdate();
-	player.camera();
-	player.checkCollisions();
-	engine.getActiveScene().display();
-	player.display();
+	engine.getActiveScene().earlyUpdate(!0);
+	player.camera(!0);
+	player.checkCollisions(!0);
+	engine.getActiveScene().display(!0);
+	player.display(!0);
 	//Late Update
-	engine.getActiveScene().lateUpdate();
+	engine.getActiveScene().lateUpdate(!0);
 	//rect(-1,375,800,500)
-}
-//Remove right click default behaviour
-canvas.oncontextmenu = function(e) {
-	e.preventDefault();
 }
